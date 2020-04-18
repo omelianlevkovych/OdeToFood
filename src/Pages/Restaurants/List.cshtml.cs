58618f1e709103af5b8aa9301f15090f658b9358
omlbucket <omelianai@gmail.com>
2020-04-18 15:55:11 +0300

    Added temporary data storage as an enumerable list in order to play a bit with razor pages. Updated gitignore

--- src/Pages/Restaurants/List.cshtml.cs
+++ src/Pages/Restaurants/List.cshtml.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
     public class ListModel : PageModel
     {
+        private readonly IRestaurantData restaurantData;
         private readonly IConfiguration config;
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(
+            IConfiguration config,
+            IRestaurantData restaurantData)
         {
+            this.restaurantData = restaurantData;
             this.config = config;
         }
 
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
